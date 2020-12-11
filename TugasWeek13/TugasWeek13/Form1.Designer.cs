9986e7758965c66947d48d75b70754b1b5f3c9eb
Christopher tekvi <christophertekvi@hotmail.com>
2020-12-11 08:53:56 +0700

    Praktikum Week 13

---
+++ TugasWeek13/TugasWeek13/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace TugasWeek13
+{
+    partial class FormPengaturanWarna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInputData = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonProses = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelInputData
+            // 
+            this.labelInputData.AutoSize = true;
+            this.labelInputData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInputData.Location = new System.Drawing.Point(24, 34);
+            this.labelInputData.Name = "labelInputData";
+            this.labelInputData.Size = new System.Drawing.Size(89, 20);
+            this.labelInputData.TabIndex = 0;
+            this.labelInputData.Text = "Input Data:";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(28, 57);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(335, 20);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // buttonProses
+            // 
+            this.buttonProses.Location = new System.Drawing.Point(28, 112);
+            this.buttonProses.Name = "buttonProses";
+            this.buttonProses.Size = new System.Drawing.Size(75, 23);
+            this.buttonProses.TabIndex = 2;
+            this.buttonProses.Text = "Proses";
+            this.buttonProses.UseVisualStyleBackColor = true;
+            this.buttonProses.Click += new System.EventHandler(this.buttonProses_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutput.Location = new System.Drawing.Point(22, 169);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(123, 31);
+            this.labelOutput.TabIndex = 3;
+            this.labelOutput.Text = "[EMPTY]";
+            // 
+            // FormPengaturanWarna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonProses);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelInputData);
+            this.Name = "FormPengaturanWarna";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInputData;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonProses;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ TugasWeek13/TugasWeek13/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasWeek13
+{
+    public partial class FormPengaturanWarna : Form
+    {
+        public FormPengaturanWarna()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonProses_Click(object sender, EventArgs e)
+        {
+            if (labelOutput.Text == "[EMPTY]")
+            {
+                labelOutput.Text = textBoxInput.Text;
+            }
+            if (labelOutput.Text != "[EMPTY]" && textBoxInput.Text == "DELETE")
+            {
+                labelOutput.Text = "[EMPTY]";
+            }
+            if (labelOutput.Text != "[EMPTY]")
+            {
+                if (textBoxInput.Text == "SHOWN")
+                {
+                    labelOutput.Visible = true;
+                }
+                if (textBoxInput.Text == "HIDE")
+                {
+                    labelOutput.Visible = false;
+                }
+            }
+            if (labelOutput.Text != "[EMPTY]")
+            {
+                if (textBoxInput.Text == "BLUE")
+                {
+                    labelOutput.ForeColor = Color.Blue;
+                }
+                if (textBoxInput.Text == "RED")
+                {
+                    labelOutput.ForeColor = Color.Red;
+                }
+                if (textBoxInput.Text == "GREEN")
+                {
+                    labelOutput.ForeColor = Color.Green;
+                }
+
+            }
+        }
+    }
+}
